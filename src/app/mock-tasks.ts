42a88923d52d6e49cfbfff9fedc58a0d454426e5
Danilo Santos <artededan@protonmail.ch>
2021-08-13 07:59:32 -0300

    (task tracker) App: Adicionar simulação e interface de tarefas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+       id: 1,
+       text: 'Ler Capítulo 1 do Livro pensar com tipos',
+       day: 'June 21th at 3:30pm',
+       reminder: true, 
+    },
+    {
+        id: 2,
+        text: 'Assistir Netflix com a namo',
+        day: 'June 22th at 22:30pm',
+        reminder: true, 
+     },
+     {
+        id: 3,
+        text: 'Comer milho com a Taylor Swift',
+        day: 'May 23th at 1:30pm',
+        reminder: false, 
+     },
+];
